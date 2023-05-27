bfe79c2f07c4b85df202bff9dbeb2a3783856d8c
tiago <tiagosantos6303@gmail.com>
2023-05-26 21:49:40 -0500

    config jotai storage

---
+++ src/app/Hooks/useLocalData.tsx
@@ -0,0 +1,20 @@
+import { useAtom } from "jotai"
+import { atomWithStorage } from "jotai/utils"
+import { testInitialValue } from "../types"
+
+const initialValues: testInitialValue = {
+    valueOne: '',
+    valueTwo: ''
+}
+
+const localDataAtom = atomWithStorage<testInitialValue>('key', initialValues)
+
+export const useLocalData = () => {
+    const [localData, setLocalData] = useAtom(localDataAtom)
+
+    function updateLocalData(newData: testInitialValue) {
+        setLocalData(newData)
+    }
+
+    return { valueOne: localData.valueOne, valueTwo: localData.valueTwo, updateLocalData}
+}
